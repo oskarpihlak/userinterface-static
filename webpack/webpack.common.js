4dfae9d0cf4ac047c437d7b528c1084e94f62aab
OskarPihlak <oskar.pihlak@gmail.com>
2020-02-27 12:25:21 +0200

    message

---
+++ webpack/webpack.common.js
@@ -0,0 +1,61 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Try the environment variable, otherwise use root
+const ASSET_PATH = process.env.ASSET_PATH || '/public/';
+
+module.exports = {
+  entry: {
+    app: Path.resolve(__dirname, '../src/js/index.js')
+  },
+  output: {
+    path: Path.join(__dirname, '../build'),
+    filename: 'js/[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false
+    }
+  },
+
+  plugins: [
+    // This makes it possible for us to safely use env vars on our code
+    // new Webpack.DefinePlugin({
+    //   'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
+    // }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: Path.resolve(__dirname, '../public'), to: 'public' }
+    ]),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/frontend/index.html')
+    })
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto'
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]'
+          }
+        }
+      },
+    ]
+  }
+};
